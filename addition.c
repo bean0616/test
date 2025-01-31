36f18891bfae3e230bcf1a58d8af5337b68ae650
Kyungbin Min <kbmin2711@gmail.com>
2025-01-31 20:56:08 +0900

    Add subtraction function and change header file name

--- addition.c
+++ addition.c
@@ -1,4 +1,4 @@
-#include "addition.h"
+#include "basic_operation.h"
 
 int add(int a, int b) {
     return a + b;
---
+++ subtraction.c
@@ -0,0 +1,5 @@
+#include "basic_operation.h"
+
+int subtraction(int a, int b) {
+    return a - b;
+}
